7d90ef29b4b37d7e9d378bac1a63689c93c2f85a
mrharshdalal <dalalharsh1974@gmail.com>
2024-01-18 11:23:16 +0530

    add

--- api/product/list/index.ts
+++ api/product/list/index.ts
@@ -3,5 +3,5 @@ import {Request, Response} from "express";
 export default async (req:Request, res:Response) => {
     const {page, limit} = req.query;
 
-    res.json({message: "Product api working properly", data: [page, limit]});
+    res.json({message: "Product api working properly"});
 };
